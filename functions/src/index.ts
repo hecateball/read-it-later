fc4831bb2f12f900e033e89a7fad51fdba8f6507
hecateball <hecateball@gmail.com>
2019-02-12 09:32:50 +0900

    create user function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,16 @@
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'render') {
   exports.render = require('./render')
 }
 
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'openGraph') {
   exports.openGraph = require('./open-graph')
+}
+
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'auth-createUser') {
+  exports.auth = exports.auth || {}
+  exports.auth.createUser = require('./auth/create-user')
 }
